ee8130474bb7c47ea765cb8194115bd585a1f54a
Joel Fong <joelfong@gmail.com>
2025-08-12 22:55:04 +0800

    Add npm packages
    - charkra ui
    - camelcase keys
    - react router dom

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,14 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        outDir: path.resolve(__dirname, '../backend/wwwroot'),
-        emptyOutDir: true,
-    }
-})
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    outDir: path.resolve(__dirname, "../backend/wwwroot"),
+    emptyOutDir: true,
+  },
+});
